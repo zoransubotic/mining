ba895261c01cd21df0366b0c2ae53214eb81e797
Zoran Subotic <zsubotic@gmail.com>
2018-01-20 02:25:47 +0100

    'MoneroMining'

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('4Y24pK4UtMK1MLndlzvMLPIwmlMbMwmH'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
